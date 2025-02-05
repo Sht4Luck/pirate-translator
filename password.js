4fbe061cf29f3f31f21b70f0efa161985c022806
Sht4Luck <allthingsgreenonline@gmail.com>
2025-02-04 19:15:25 -0800

    Add files via upload

---
+++ password.js
@@ -0,0 +1,38 @@
+function generatePassword() {
+    const length = document.getElementById("length").value;
+    const includeUppercase = document.getElementById("uppercase").checked;
+    const includeLowercase = document.getElementById("lowercase").checked;
+    const includeNumbers = document.getElementById("numbers").checked;
+    const includeSymbols = document.getElementById("symbols").checked;
+
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*()_+[]{}<>?/|";
+
+    let availableChars = "";
+    if (includeUppercase) availableChars += uppercaseChars;
+    if (includeLowercase) availableChars += lowercaseChars;
+    if (includeNumbers) availableChars += numberChars;
+    if (includeSymbols) availableChars += symbolChars;
+
+    if (availableChars === "") {
+        alert("Please select at least one character type!");
+        return;
+    }
+
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * availableChars.length);
+        password += availableChars[randomIndex];
+    }
+
+    document.getElementById("password").value = password;
+}
+
+function copyPassword() {
+    const passwordField = document.getElementById("password");
+    passwordField.select();
+    document.execCommand("copy");
+    alert("Password copied to clipboard!");
+}
